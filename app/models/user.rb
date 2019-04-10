9c1bf83584c472c12a36bc519ceb5ea2f6247b41
kypkyp <kyp@kmc.gr.jp>
2019-04-10 13:40:12 +0900

    fuck twitter

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
-         :confirmable, :lockable, :timeoutable,
-         :omniauthable, omniauth_providers: [:twitter]
+         :recoverable, :rememberable, :validatable
+         #:confirmable, :lockable, :timeoutable #,
+         #:omniauthable, omniauth_providers: [:twitter]
 end
