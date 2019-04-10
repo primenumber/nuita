87ae4e2bb5c20a41d93eef8436692262812a0d37
kypkyp <kyp@kmc.gr.jp>
2019-04-10 15:44:12 +0900

    validate user names

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
          #:confirmable, :lockable, :timeoutable #,
          #:omniauthable, omniauth_providers: [:twitter]
+
+  validates :screen_name, presence: true, uniqueness: true, length: {maximum: 20}
+  validates :screen_name, format: {with: /[0-9a-zA-Z_]/}
+  validates :handle_name, length: {maximum: 30}
 end
