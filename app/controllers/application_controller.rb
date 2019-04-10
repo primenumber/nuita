92b5300c7577f8f7b10f0d38f8ef55af24358aba
kypkyp <kyp@kmc.gr.jp>
2019-04-10 15:45:07 +0900

    add names to forms

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+    # strong parameters
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:handle_name, :screen_name])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:handle_name, :screen_name])
+    end
 end
