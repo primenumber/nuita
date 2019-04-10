341f52251ca124294078028e8e15223069b30532
kypkyp <kyp@kmc.gr.jp>
2019-04-10 11:55:35 +0900

    add twitter columns to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_10_024927) do
+ActiveRecord::Schema.define(version: 2019_04_10_025504) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -32,6 +32,9 @@ ActiveRecord::Schema.define(version: 2019_04_10_024927) do
     t.datetime "locked_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "provider"
+    t.string "uid"
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
