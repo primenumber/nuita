10b18da5de8654ec16cf4a9ccdf4072caffbb51a
kypkyp <kyp@kmc.gr.jp>
2019-04-11 16:55:14 +0900

    雑にshowページ作った

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/show'
   devise_for :users
   root 'pages#home'
   get 'pages/home'
   get 'pages/about'
+
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
