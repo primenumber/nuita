0f5a1649e831fc140b9e7bdfbf3d596f46b6f827
kypkyp <kyp@kmc.gr.jp>
2019-04-14 14:00:20 +0900

    create timeline view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,8 @@
 class PagesController < ApplicationController
   def home
+    if user_signed_in?
+      @feed_items = current_user.timeline.paginate(page: params[:page])
+    end
   end
 
   def about
--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,10 @@ class User < ApplicationRecord
   validates :screen_name, format: {with: /[0-9a-zA-Z_]/}
   validates :handle_name, length: {maximum: 30}
 
+  def timeline
+    Nweet.where("user_id = ?", id)
+  end
+
   class << self
     def screen_name_formatter(str)
       str.gsub(/\W/, '_')[0...20]
