b9b50f09bf52b5e86aaeeef5e782819898a62494
kypkyp <kyp@kmc.gr.jp>
2019-04-30 18:15:07 +0900

    reflect button-status without JS

--- app/assets/javascripts/pages.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function(){
-  document.getElementById('button-nuita').addEventListener('click', function(){
-    this.className = "btn btn-block btn-complete";
-    this.value = "おめでとうございます🎉🎉🎉🎉🎉🎉";
-    // this.setAttribute("disabled", "disabled");
-  });
-}, false);
