c431b201217f8bd1c4c4a60fec0c0c3438149d35
kypkyp <kyp@kmc.gr.jp>
2019-04-30 18:09:02 +0900

    fix bug that first nweet is disabled

--- app/models/nweet.rb
+++ app/models/nweet.rb
@@ -9,14 +9,14 @@ class Nweet < ApplicationRecord
 
   def did_at_past?
     if did_at # did at is not nil
-      errors.add(:did_at, "is not in the past") unless did_at <= Time.zone.now
+      errors.add(:did_at, " is not in the past") unless did_at <= Time.zone.now
     end
   end
 
   # may need refactoring
   def have_enough_interval?
-    if user.nweets.first && did_at && did_at < user.nweets.first.did_at + 3.minutes
-      errors.add(:did_at, "has not enough interval")
+    if user.nweets.count != 0 && did_at && did_at < user.nweets.first.did_at + 3.minutes
+      errors.add(:did_at, " has not enough interval")
     end
   end
 
