89140fb50102053b3fd03d56e2ee9d1c77dda01e
kypkyp <kyp@kmc.gr.jp>
2019-05-02 17:06:16 +0900

    タイムラインのviewにicon追加

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,4 +3,14 @@ module UsersHelper
   def self.screen_name_formatter(str)
     str.gsub(/ /, '_')
   end
+
+  # iconのイメージ返す　設定なしならデフォルト
+  def icon_for(user, size: 80)
+    if user.icon.url.present?
+      url = user.icon.url
+    else
+      url = asset_path('icon_default')
+    end
+    image_tag(url, alt: user.handle_name, size: size.to_s, class:'usericon', id: "user-#{user.id}")
+  end
 end
