3928ad11b0aea59b9bf4f354ad00047b64d27d9a
kypkyp <kyp@kmc.gr.jp>
2019-05-30 22:51:18 +0900

    viewから連携解除できる

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -3,4 +3,10 @@ class OmniauthCallbacksController < ApplicationController
     current_user.add_twitter_account(request.env['omniauth.auth'])
     redirect_to edit_user_registration_path
   end
+
+  # CRUD...
+  def delete_twitter
+    current_user.delete_twitter_account
+    redirect_to edit_user_registration_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get 'pages/about'
   get '/auth/twitter/callback', :to => 'omniauth_callbacks#twitter'
   post '/auth/twitter/callback', :to => 'omniauth_callbacks#twitter'
+  delete '/auth/twitter', :to => 'omniauth_callbacks#delete_twitter'
 
   resources :users, except: [:index], param: :url_digest
   resources :nweets, only: [:create, :update, :destroy]
