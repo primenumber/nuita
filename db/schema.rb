9da1fd41f521379cbec6c84a3817a5bde3775900
kypkyp <kyp@kmc.gr.jp>
2019-06-12 17:31:37 +0900

    関係性テーブル

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_01_231324) do
+ActiveRecord::Schema.define(version: 2019_06_12_082409) do
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "nweet_id"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["nweet_id"], name: "index_favorites_on_nweet_id"
+    t.index ["user_id"], name: "index_favorites_on_user_id"
+  end
 
   create_table "nweets", force: :cascade do |t|
     t.datetime "did_at"
