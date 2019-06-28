1f4a8f58d86417bdb218389d95dab9863edd6bc2
kypkyp <kyp@kmc.gr.jp>
2019-06-29 07:59:32 +0900

    いいねしたときにフラッシュメッセージ見せる

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,6 +2,7 @@ class FavoritesController < ApplicationController
   def create
     nweet = Nweet.find_by(url_digest: params[:nweet])
     @favorite = current_user.favorites.create(nweet_id: nweet.id)
+    flash[:success] = nweet.user.handle_name + 'さんのヌイートをいいねしました！'
     redirect_back(fallback_location: root_path)
   end
 
@@ -9,6 +10,7 @@ class FavoritesController < ApplicationController
     nweet = Nweet.find_by(url_digest: params[:nweet])
     @favorite = Favorite.find_by(nweet_id: nweet.id, user_id: current_user.id)
     @favorite.destroy
+    flash[:danger] = 'ヌイートのいいねを解除しました……'
     redirect_back(fallback_location: root_path)
   end
 end
