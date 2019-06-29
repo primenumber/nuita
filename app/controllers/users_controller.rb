20ec4961944b6254dbdad37baca8a8f15fa256ff
kypkyp <kyp@kmc.gr.jp>
2019-06-29 21:53:08 +0900

    他のユーザーのいいねが見られるようになった

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,9 @@ class UsersController < ApplicationController
     @user = User.find_by(url_digest: params[:url_digest])
     @nweets = @user.nweets.paginate(page: params[:page], per_page: 50)
   end
+
+  def likes
+    @user = User.find_by(url_digest: params[:url_digest])
+    @feed_items = @user.fav_nweets.paginate(page: params[:page], per_page: 100)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,18 @@ Rails.application.routes.draw do
   devise_for :users
   root 'pages#home'
   get 'pages/home'
-  get '/i/likes', :to => 'pages#favorites', as: 'likes'
   get '/i/notifications', :to=> 'pages#notifications', as: 'notifications'
   get 'pages/about'
   get '/auth/twitter/callback', :to => 'twitters#create'
   post '/auth/twitter/callback', :to => 'twitters#create'
   delete '/auth/twitter', :to => 'twitters#destroy'
 
-  resources :users, except: [:index], param: :url_digest
+  resources :users, except: [:index], param: :url_digest do
+    member do
+      get :likes
+    end
+  end
+
   resources :nweets, only: [:create, :update, :destroy, :show], param: :url_digest
   resource :favorite, only: [:create, :destroy]
 end
