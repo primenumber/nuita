5e0c86d472c0a25022dc36f1ce41b9d2955a22ca
kypkyp <kyp@kmc.gr.jp>
2019-07-03 08:54:02 +0900

    他人のいいね見られなくした

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, only: [:likes]
+
   def show
     @user = User.find_by(url_digest: params[:url_digest])
     @nweets = @user.nweets.paginate(page: params[:page], per_page: 50)
@@ -6,6 +8,9 @@ class UsersController < ApplicationController
 
   def likes
     @user = User.find_by(url_digest: params[:url_digest])
+
+    redirect_to root_url unless @user == current_user
+
     @feed_items = @user.fav_nweets.paginate(page: params[:page], per_page: 100)
   end
 end
