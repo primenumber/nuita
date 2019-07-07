2f35dadf527f4bb8b237ea280c9d342df8b0b9c0
kypkyp <kyp@kmc.gr.jp>
2019-07-07 11:28:08 +0900

    いいね時のflashメッセージ削除（Ajaxだとうまくいかない & そもそも画面上端見ないので）

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,7 +2,7 @@ class FavoritesController < ApplicationController
   def create
     nweet = Nweet.find_by(url_digest: params[:nweet])
     @favorite = current_user.favorites.create(nweet_id: nweet.id)
-    flash[:success] = nweet.user.handle_name + 'さんのヌイートをいいねしました！'
+    # flash[:success] = nweet.user.handle_name + 'さんのヌイートをいいねしました！'
     head :no_content
   end
 
@@ -10,7 +10,7 @@ class FavoritesController < ApplicationController
     nweet = Nweet.find_by(url_digest: params[:nweet])
     @favorite = Favorite.find_by(nweet_id: nweet.id, user_id: current_user.id)
     @favorite.destroy
-    flash[:danger] = 'ヌイートのいいねを解除しました……'
+    # flash[:danger] = 'ヌイートのいいねを解除しました……'
     head :no_content
   end
 end
