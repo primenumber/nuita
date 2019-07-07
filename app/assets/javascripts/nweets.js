7bcfe2f7a6d259ae7e2f0d9dc38457a32155168e
kypkyp <kyp@kmc.gr.jp>
2019-07-07 16:08:18 +0900

    .nweet内にいいねしたときのメッセージを追加

--- app/assets/javascripts/nweets.js
+++ app/assets/javascripts/nweets.js
@@ -1,17 +1,20 @@
-// add likes
 document.addEventListener('turbolinks:load', function(){
   document.querySelectorAll('.fav-btn').forEach(function(div){
     div.addEventListener('ajax:success', function(){
       var a = div.firstElementChild;
-      var i = a.firstElementChild;
+      var i = a.childNodes[0];
+      var span = div.childNodes[1];
       i.classList.toggle('faved');
 
       if(i.classList.contains('fa-heart')){
         i.classList.replace('fa-heart', 'fa-heart-o');
         a.setAttribute('data-method', 'post');
+        span.classList.remove('faved-flash')
       }else{
         i.classList.replace('fa-heart-o', 'fa-heart');
         a.setAttribute('data-method', 'delete');
+        span.innerHTML = 'いいねしました！'
+        span.classList.add('faved-flash')
       }
     })
   });
