dc2cf874f153d11d42dd8a694c793067dc23ffbd
kypkyp <kyp@kmc.gr.jp>
2019-08-12 15:54:13 +0900

    user_info, 今表示してるページのバッジの見た目を変える

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,1 +1,18 @@
+document.addEventListener('turbolinks:load', function(){
+  var pathname = location.pathname;
+  var element;
 
+  if (~pathname.indexOf('likes')){
+    element = document.getElementById('badgeLikes');
+  }else if(~pathname.indexOf('followers')){
+    element = document.getElementById('badgeFollowers');
+  }else if(~pathname.indexOf('followees')){
+    element = document.getElementById('badgeFollowees');
+  }else if(~pathname.indexOf('users')){
+    element = document.getElementById('badgeNweets');
+  }
+
+  if(!!element){
+    element.classList.add('active');
+  }
+});
