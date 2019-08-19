885446ad4cda2823f60083224745834f3bf7c630
kypkyp <kyp@kmc.gr.jp>
2019-08-19 22:48:50 +0900

    おっきいボタンはajax通信じゃないからhtmlも返せるようにしとく

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -4,12 +4,20 @@ class RelationshipsController < ApplicationController
   def create
     user = User.find_by(url_digest: params[:followee])
     current_user.follow(user)
-    head :no_content
+    if request.xhr?
+      head :no_content
+    else
+      redirect_back(fallback_location: root_path)
+    end
   end
 
   def destroy
     user = User.find_by(url_digest: params[:followee])
     current_user.unfollow(user)
-    head :no_content
+    if request.xhr?
+      head :no_content
+    else
+      redirect_back(fallback_location: root_path)
+    end
   end
 end
