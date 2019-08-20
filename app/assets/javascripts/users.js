c88d3faac655f9342760d30f854c951e1cee1f3d
kypkyp <kyp@kmc.gr.jp>
2019-08-20 20:35:25 +0900

    date表示のときカレンダーの該当部分を大きくする

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -2,14 +2,20 @@ document.addEventListener('turbolinks:load', function(){
   var pathname = location.pathname;
   var element;
 
-  if (~pathname.indexOf('likes')){
-    element = document.getElementById('badgeLikes');
-  }else if(~pathname.indexOf('followers')){
-    element = document.getElementById('badgeFollowers');
-  }else if(~pathname.indexOf('followees')){
-    element = document.getElementById('badgeFollowees');
-  }else if(~pathname.indexOf('users')){
-    element = document.getElementById('badgeNweets');
+  if(~(m = location.search.match(/\d{4}%2F\d{2}%2F\d{2}/))){
+    element = document.getElementById('cal' + unescape(m));
+  }else{
+    if (~pathname.indexOf('likes')){
+      element = document.getElementById('badgeLikes');
+    }else if(~pathname.indexOf('followers')){
+      element = document.getElementById('badgeFollowers');
+    }else if(~pathname.indexOf('followees')){
+      element = document.getElementById('badgeFollowees');
+    }else if(~pathname.indexOf('users')){
+      element = document.getElementById('badgeNweets');
+    }else{
+      element = null
+    }
   }
 
   if(!!element){
