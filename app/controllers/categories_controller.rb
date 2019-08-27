d37d6219ace2785fb442638880d2a26bba30f05e
kypkyp <kyp@kmc.gr.jp>
2019-08-27 22:00:19 +0900

    このPRでは特定タグに絞って、付け外しだけできるようにしておきます

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,10 +4,22 @@ class CategoriesController < ApplicationController
   def create
     link = Link.find(params[:link_id])
     link.set_category(params[:category_name])
+
+    if request.xhr?
+      head :no_content
+    else
+      redirect_back(fallback_location: root_path)
+    end
   end
 
   def destroy
     link = Link.find(params[:link_id])
     link.remove_category(params[:category_name])
+
+    if request.xhr?
+      head :no_content
+    else
+      redirect_back(fallback_location: root_path)
+    end
   end
 end
