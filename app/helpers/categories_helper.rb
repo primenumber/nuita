2801e3e240ab6594b84483d3dbdf5a521a8db14f
kypkyp <kyp@kmc.gr.jp>
2019-08-27 22:59:04 +0900

    censoredなタグついてるやつを展開表示

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,2 +1,10 @@
 module CategoriesHelper
+  def censored?(link)
+    names = link.categories.where(censored_by_default: true).pluck(:name)
+    if names.empty? #これもっとうまく書けるきがする
+      false
+    else
+      names
+    end
+  end
 end
