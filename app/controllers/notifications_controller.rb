1c2716007bfb6176ad2f7fdb2582f753b602e69a
kypkyp <kyp@kmc.gr.jp>
2019-09-18 23:18:57 +1100

    いいねの通知機能完成

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -2,5 +2,6 @@ class NotificationsController < ApplicationController
   before_action :authenticate_user!, only: [:index]
 
   def index
+    @notifications = current_user.passive_notifications.paginate(page: params[:page], per_page: 100)
   end
 end
