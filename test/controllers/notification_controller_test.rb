5dd8ebf5ceff48ec546829b75a6bd71c5002a967
kypkyp <kyp@kmc.gr.jp>
2019-09-21 10:36:48 +1100

    久々にちゃんとビューのテスト書いた気がする

--- test/controllers/notification_controller_test.rb
+++ test/controllers/notification_controller_test.rb
@@ -6,6 +6,8 @@ class NotificationControllerTest < ActionDispatch::IntegrationTest
   def setup
     Warden.test_mode!
     @user = users(:chikuwa)
+    @other_user = users(:emiya)
+    @nweet = nweets(:saytwo)
   end
 
   test "should get index when logged-in" do
@@ -17,4 +19,17 @@ class NotificationControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test "should count unread notifications" do
+    c = @other_user.passive_notifications.where(checked: false).count
+
+    login_as(@user)
+    post like_path, params: {nweet: @nweet.url_digest}
+
+    login_as(@other_user)
+    get root_url
+    assert_select "#badgeUnreadNotifications", (c + 1).to_s
+
+    get notifications_path
+    assert_select "#badgeUnreadNotifications", false
+  end
 end
