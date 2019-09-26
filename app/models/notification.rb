0ba6f445e1e3f7a90b1a4d057c386d92bb6214f5
kypkyp <kyp@kmc.gr.jp>
2019-09-26 20:08:50 +0900

    通知の種類にAnnounceを追加、表示

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,6 +1,6 @@
 class Notification < ApplicationRecord
   default_scope->{order(created_at: :desc)}
-  enum action: [:like, :follow]
+  enum action: [:like, :follow, :announce]
 
   belongs_to :like, optional: true
   belongs_to :origin, class_name: 'User', optional: true
