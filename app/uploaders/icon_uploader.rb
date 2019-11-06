fecda78feb4e7e853634349492c13c8b3da1b7a2
kypkyp <kyp@kmc.gr.jp>
2019-11-06 11:38:09 +0900

    アイコンアップロード時に強制リサイズ、thumbサイズも作る

--- app/uploaders/icon_uploader.rb
+++ app/uploaders/icon_uploader.rb
@@ -1,7 +1,7 @@
 class IconUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   if Rails.env.production?
@@ -27,14 +27,12 @@ class IconUploader < CarrierWave::Uploader::Base
   # Process files as they are uploaded:
   # process scale: [200, 300]
   #
-  # def scale(width, height)
-  #   # do something
-  # end
-
+  process resize_to_fit: [256, 256]
+  
   # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process resize_to_fit: [50, 50]
-  # end
+  version :thumb do
+    process resize_to_fit: [60, 60]
+  end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
