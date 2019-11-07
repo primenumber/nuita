9f17cb0960a71cdfbd01821359ebf07f435d6d2f
kypkyp <kyp@kmc.gr.jp>
2019-11-07 18:20:42 +0900

    users#censoringsで検閲カテゴリの再設定することにする. deviseのフォームとは分けたかった

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,6 +29,23 @@ class UsersController < ApplicationController
     render 'show_follow'
   end
 
+  def censorings
+    begin
+      Category.pluck(:name).each do |category_name|
+        if current_user.censoring?(category_name)
+          current_user.uncensor(category_name) if params[category_name] == '0'
+        else
+          current_user.censor(category_name) if params[category_name] == '1'
+        end
+      end
+      flash[:success] = '検閲カテゴリの設定に成功しました'
+    rescue => e
+      flash[:danger] = "検閲カテゴリの設定に失敗しました: #{e}"
+    ensure
+      redirect_to edit_user_registration_path(current_user)
+    end
+  end
+
   private
 
     def friend_user
--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ Rails.application.routes.draw do
     member do
       get :likes
       get :followers, :followees
+      put :censorings
     end
   end
 
