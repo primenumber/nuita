5b7852155d0fde2772ef7d015a9ac35f624ede20
kypkyp <kyp@kmc.gr.jp>
2019-11-28 05:21:52 +0900

    クリスマスカレンダー仮作成. 右側に置く

--- app/helpers/christmas_helper.rb
+++ app/helpers/christmas_helper.rb
@@ -1,2 +1,8 @@
 module ChristmasHelper
+  # 再利用不可能
+  def christmas_stamps_for(user, calendar_range)
+    stamps = user.stamps.where(:targeted_at => calendar_range)
+
+    calendarize_data(stamps, column: :targeted_at)
+  end
 end
